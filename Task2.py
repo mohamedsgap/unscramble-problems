e007ac54d436f8f5ecbc0e8ee0e0e3fa21ac27c2
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-08-07 16:08:53 +0200

    complete task2

--- Task2.py
+++ Task2.py
@@ -16,7 +16,23 @@ TASK 2: Which telephone number spent the longest time on the phone
 during the period? Don't forget that time spent answering a call is
 also time spent on the phone.
 Print a message:
-"<telephone number> spent the longest time, <total time> seconds, on the phone during 
+"<telephone number> spent the longest time, <total time> seconds, on the phone during
 September 2016.".
 """
 
+longest_num = None
+longest_duration = 0
+tel_num_duration = {}
+
+for record in calls:
+    for i in range(2):
+        tel_num = record[i]
+        duration = int(record[-1])
+
+        tel_num_duration[tel_num] =  tel_num_duration.get(tel_num, 0) + duration
+
+        if tel_num_duration[tel_num] > longest_duration:
+            longest_duration = tel_num_duration[tel_num]
+            longest_num = tel_num
+
+print("{} spent the longest time, {} seconds, on the phone during September 2016.".format(longest_num, longest_duration))
