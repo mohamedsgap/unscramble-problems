50ebe5b48517f82afeacdc8eb9de3e162053ce53
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-08-07 16:08:20 +0200

    complete task1

--- Task1.py
+++ Task1.py
@@ -14,7 +14,17 @@ with open('calls.csv', 'r') as f:
 
 """
 TASK 1:
-How many different telephone numbers are there in the records? 
+How many different telephone numbers are there in the records?
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+
+
+tel_nums = set() # unordered list with duplicate elements removed.
+
+for text, call in zip(texts, calls): # pair records from texts and calls.
+    for i in range(2): # first two columns are telephone numbers.
+        tel_nums.add(text[i])
+        tel_nums.add(call[i])
+
+print("There are {} different telephone numbers in the records.".format(len(tel_nums)))        
